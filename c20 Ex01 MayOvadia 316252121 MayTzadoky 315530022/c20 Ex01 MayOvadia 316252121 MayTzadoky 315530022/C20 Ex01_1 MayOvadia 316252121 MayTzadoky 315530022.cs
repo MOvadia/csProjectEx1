2eab4fd48db5707731fc2161ae924c1fb5a2f22c
May <May.Ovadia@verint.com>
2020-08-09 23:09:31 +0300

    add project

---
+++ c20 Ex01 MayOvadia 316252121 MayTzadoky 315530022/c20 Ex01 MayOvadia 316252121 MayTzadoky 315530022/C20 Ex01_1 MayOvadia 316252121 MayTzadoky 315530022.cs
@@ -0,0 +1,10 @@
+﻿namespace c20_Ex01_MayOvadia_316252121_MayTzadoky_315530022
+{
+    class C20_Ex01_1_MayOvadia_316252121_MayTzadoky_315530022
+    {
+        static void Main()
+        {
+            //data
+        }
+    }
+}
---
+++ c20 Ex01 MayOvadia 316252121 MayTzadoky 315530022/c20 Ex01 MayOvadia 316252121 MayTzadoky 315530022/c20 Ex01_2 MayOvadia 316252121 MayTzadoky 315530022.cs
@@ -0,0 +1,10 @@
+﻿namespace C20_Ex01_MayOvadia_316252121_MayTzadoky_315530022
+{
+    class C20_Ex01_2_MayOvadia_316252121_MayTzadoky_315530022
+    {
+        static void Main()
+        {
+            //data
+        }
+    }
+}
