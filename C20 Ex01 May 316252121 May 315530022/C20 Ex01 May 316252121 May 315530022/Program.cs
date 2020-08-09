e3f7bc3a16e3f5429b1448d6e1bafa17272f1e93
May <May.Ovadia@verint.com>
2020-08-10 00:06:54 +0300

    add project

---
+++ C20 Ex01 May 316252121 May 315530022/C20 Ex01 May 316252121 May 315530022/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C20_Ex01_1_MayOvadia_316252121_MayTzadoky_315530022
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ C20 Ex01 May 316252121 May 315530022/C20 Ex01_5 MayOvadia 316252121 MayTzadoky 315530022/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C20_Ex01_5_MayOvadia_316252121_MayTzadoky_315530022
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
