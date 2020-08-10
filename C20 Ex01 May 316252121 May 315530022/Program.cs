d1cdc9281ee59853f0c6dc60c280a51c98717505
May <May.Ovadia@verint.com>
2020-08-10 12:20:56 +0300

    start 1 and finished 2

--- C20 Ex01 May 316252121 May 315530022/Program.cs
+++ C20 Ex01 May 316252121 May 315530022/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace C20_Ex01_1_MayOvadia_316252121_MayTzadoky_315530022
+﻿
+
+namespace C20_Ex01_1_MayOvadia_316252121_MayTzadoky_315530022
 {
     class Program
     {
         public static void Main()
         {
+        }
+
+        public static void GetUserInput()
+        {
+            string binaryNumber1, binaryNumber2, binaryNumber3, binaryNumber4;
+
+            System.Console.WriteLine("Please enter 4 binaries numbers:");
+            for (int i = 0; i < 4; ++i)
+            {
+                binaryNumber1 = System.Console.ReadLine();
+            }
 
         }
+
     }
 }
--- C20 Ex01_2 MayOvadia 316252121 MayTzadoky 315530022/Program.cs
+++ C20 Ex01_2 MayOvadia 316252121 MayTzadoky 315530022/Program.cs
@@ -4,7 +4,46 @@
     {
         public static void Main()
         {
+            PrintHourglassRec(0);
+        }
 
+        public static void PrintHourglassRec(int i_numOfSpaces)
+        {
+            bool isSingleStar = i_numOfSpaces >= 2;
+
+            if (isSingleStar)
+            {
+                PrintStarsLine(i_numOfSpaces);
+                return;
+            }
+            PrintStarsLine(i_numOfSpaces);
+
+            PrintHourglassRec(i_numOfSpaces + 1);
+
+            PrintStarsLine(i_numOfSpaces);
+        }
+
+        public static void PrintStarsLine(int i_numOfSpaces)
+        {
+            int numOfStars = 5 - i_numOfSpaces * 2;
+
+            PrintSpaces(i_numOfSpaces);
+
+            for (int i = 0; i < numOfStars; ++i)
+            {
+                System.Console.Write("*");
+            }
+            PrintSpaces(i_numOfSpaces);
+
+            System.Console.Write("\n");
+        }
+
+        public static void PrintSpaces(int i_numOfSpaces)
+        {
+            for (int i = 0; i < i_numOfSpaces; ++i)
+            {
+                System.Console.Write(" ");
+            }
         }
     }
 }
