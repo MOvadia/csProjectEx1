0e65faa735a627fd8cfb9194d6dee44cdce3f09b
MayTzadoky <may.zadoki@gmail.com>
2020-08-13 17:55:17 +0300

    The beginning of 3

--- C20 Ex01_3 MayOvadia 316252121 MayTzadoky 315530022/Program.cs
+++ C20 Ex01_3 MayOvadia 316252121 MayTzadoky 315530022/Program.cs
@@ -6,12 +6,27 @@
     {
         public static void Main()
         {
+            PrintHourGlass();
+        }
+
+        public static void PrintHourGlass()
+        {
             //TODO validity check
             System.Console.WriteLine("Please enter the number of lines for the Hour glass: ");
 
-           string lengthOfHourGlass = System.Console.ReadLine();
-           int numLengthOfHourGlass =  int.Parse(lengthOfHourGlass);
-           C20_Ex01_2_MayOvadia_316252121_MayTzadoky_315530022.Program.PrintHourglassRec(numLengthOfHourGlass);
+            int lengthOfHourGlass = int.Parse(System.Console.ReadLine());
+            PrintHourGlassRec(lengthOfHourGlass);
         }
+
+        public static void PrintHourGlassRec(int i_numOfSpaces)
+        {
+            if(i_numOfSpaces == 0)
+            {
+                C20_Ex01_2_MayOvadia_316252121_MayTzadoky_315530022.Program.PrintHourglassRec(i_numOfSpaces);
+            }
+        }
+
+
+
     }
 }
