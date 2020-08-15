e931a21e72881762b836d9f46167e7330a80311f
May <May.Ovadia@verint.com>
2020-08-15 15:05:49 +0300

    change func name

--- C20 Ex01_2 MayOvadia 316252121 MayTzadoky 315530022/Program.cs
+++ C20 Ex01_2 MayOvadia 316252121 MayTzadoky 315530022/Program.cs
@@ -10,28 +10,28 @@
         public static void PrintHourGlass(int i_numOfSpaces, int i_lengthOfHourGlass)
         {
             System.Text.StringBuilder hourGlassBuild = new System.Text.StringBuilder();
-            PrintHourGlassRec(i_numOfSpaces, i_lengthOfHourGlass, ref hourGlassBuild);
+            AppendToStringHourGlassRec(i_numOfSpaces, i_lengthOfHourGlass, ref hourGlassBuild);
             System.Console.WriteLine(hourGlassBuild.ToString());
         }
 
-        public static void PrintHourGlassRec(int i_numOfSpaces, int i_lengthOfHourGlass, ref System.Text.StringBuilder io_hourGlassBuild)
+        public static void AppendToStringHourGlassRec(int i_numOfSpaces, int i_lengthOfHourGlass, ref System.Text.StringBuilder io_hourGlassBuild)
         {
             bool isSingleStar = i_lengthOfHourGlass == 1;
 
             if (isSingleStar)
             {
-                PrintStarsLine(i_numOfSpaces, i_lengthOfHourGlass, ref io_hourGlassBuild);
+                AppendStarsLine(i_numOfSpaces, i_lengthOfHourGlass, ref io_hourGlassBuild);
                 return;
             }
 
-            PrintStarsLine(i_numOfSpaces, i_lengthOfHourGlass, ref io_hourGlassBuild);
-            PrintHourGlassRec(i_numOfSpaces + 1, i_lengthOfHourGlass - 2, ref io_hourGlassBuild);
-            PrintStarsLine(i_numOfSpaces, i_lengthOfHourGlass, ref io_hourGlassBuild);
+            AppendStarsLine(i_numOfSpaces, i_lengthOfHourGlass, ref io_hourGlassBuild);
+            AppendToStringHourGlassRec(i_numOfSpaces + 1, i_lengthOfHourGlass - 2, ref io_hourGlassBuild);
+            AppendStarsLine(i_numOfSpaces, i_lengthOfHourGlass, ref io_hourGlassBuild);
         }
 
-        public static void PrintStarsLine(int i_numOfSpaces, int i_lengthOfHourGlass, ref System.Text.StringBuilder io_hourGlassBuild)
+        public static void AppendStarsLine(int i_numOfSpaces, int i_lengthOfHourGlass, ref System.Text.StringBuilder io_hourGlassBuild)
         {
-            PrintSpaces(i_numOfSpaces, ref io_hourGlassBuild);
+            AppendSpaces(i_numOfSpaces, ref io_hourGlassBuild);
 
             for (int i = 0; i < i_lengthOfHourGlass; ++i)
             {
@@ -41,7 +41,7 @@
             io_hourGlassBuild.AppendLine();
         }
 
-        public static void PrintSpaces(int i_numOfSpaces, ref System.Text.StringBuilder io_hourGlassBuild)
+        public static void AppendSpaces(int i_numOfSpaces, ref System.Text.StringBuilder io_hourGlassBuild)
         {
             for (int i = 0; i < i_numOfSpaces; ++i)
             {
